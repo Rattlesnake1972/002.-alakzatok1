a474c7276ee3ca0efae38e4b74fe777a874a1632
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-25 19:28:20 +0100

    Tíz db *-ot és tíz db o-t ír ki

---
+++ 002. alakzatok1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002.alakzatok1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < 11; i++)
+            {
+                Console.Write("o");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
